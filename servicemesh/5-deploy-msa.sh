c97336cff3a1720ad4a1465d424da643121c2cb0
cvicens <carlos.vicens.alonso@gmail.com>
2019-03-18 12:51:45 +0100

    Cleaning

--- servicemesh/5-deploy-msa.sh
+++ servicemesh/5-deploy-msa.sh
@@ -5,11 +5,12 @@
 
 oc new-project ${MSA_PROJECT_NAME}
 
+# There no resource limits in our msa... so just in case delelte limits in our namespace
+oc delete limitrange --all -n ${MSA_PROJECT_NAME}
+
 oc adm policy add-scc-to-user privileged -z default -n ${MSA_PROJECT_NAME}
 
-GIT_URI="https://github.com/rmarting/service-mesh-meetup"
-GIT_REF="master"
 COOLSTORE_GW_ENDPOINT="http://$(oc get route istio-ingressgateway -n ${ISTIO_SYSTEM_NAMESPACE} | awk 'NR>1 {print $2}')"
 
-oc process -p GIT_URI=${GIT_URI} -p GIT_REF=${GIT_REF} -p COOLSTORE_GW_ENDPOINT=${COOLSTORE_GW_ENDPOINT} -f ../microservices/coolstore-msa-template.yaml | \
+oc process -p GIT_URI=${MSA_GIT_URI} -p GIT_REF=${MSA_GIT_REF} -p COOLSTORE_GW_ENDPOINT=${COOLSTORE_GW_ENDPOINT} -f ../microservices/coolstore-msa-template.yaml | \
   oc apply -n ${MSA_PROJECT_NAME} -f -
