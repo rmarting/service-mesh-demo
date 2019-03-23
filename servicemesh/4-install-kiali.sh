56f3aa2aa8b9d607cb256f2da7a05358942d14bb
cvicens <carlos.vicens.alonso@gmail.com>
2019-03-23 18:56:01 +0100

    adding NAMESPACE env variable

--- servicemesh/4-install-kiali.sh
+++ servicemesh/4-install-kiali.sh
@@ -7,6 +7,7 @@ echo "Deploy Kiali Definitions ..."
 
 export JAEGER_URL="$(oc get route jaeger-query -n ${ISTIO_SYSTEM_NAMESPACE} | awk 'NR>1 {printf ($5 == "edge") ? "https://%s" : "http://%s",$2 }')"
 export GRAFANA_URL="$(oc get route grafana -n ${ISTIO_SYSTEM_NAMESPACE} | awk 'NR>1 {printf ($5 == "edge") ? "https://%s" : "http://%s",$2 }')" 
+export NAMESPACE=${ISTIO_SYSTEM_NAMESPACE}
 
 for yaml in secret configmap serviceaccount clusterrole clusterrolebinding deployment service route ingress crds
 do
