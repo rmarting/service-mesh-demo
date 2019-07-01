ce1b2b7c247d624f25116ee78b947f113a3c0f9a
Roman Martin <rmarting@redhat.com>
2019-07-01 11:06:31 +0200

    Throwing exception when inventory fails

--- microservices/gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
+++ microservices/gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
@@ -82,14 +82,14 @@ public class GatewayVerticle extends AbstractVerticle {
                             }
                             return newProduct;
                         } 
-                        else {
+
+                        LOG.warn("Inventory error for {}: status code {}", product.getString("itemId"), resp.statusCode());
+                        // return product.copy().put("availability", new JsonObject().put("quantity", -1));
+
                             //throw new RuntimeException("Invalid response from the catalog: " + resp.statusCode());
                             throw new HttpRuntimeException(
                                 "Invalid response from the inventary: " + resp.statusCode(),
-                                resp.statusCode());
-                        }
-                        LOG.warn("Inventory error for {}: status code {}", product.getString("itemId"), resp.statusCode());
-                        return product.copy().put("availability", new JsonObject().put("quantity", -1));
+                                resp.statusCode());                                                
                     })
                     /*.subscribe(
                         json -> LOG.info("json: " + json),
